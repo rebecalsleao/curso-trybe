110664ebc40d138e9a5d84bcb1196b219a86524e
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-08-18 15:13:40 -0300

    Exercício de fixação If/else

---
+++ Fundamentos/bloco_04_JS/Exercício_Dia_01/Parte_03/script.js
@@ -0,0 +1,19 @@
+// const notaCandidato = 87;
+
+// if (notaCandidato >= 80) {
+//     console.log("Parabéns, você foi aprovada(o)!");
+// }
+// if (notaCandidato < 80 && notaCandidato >= 60) {
+//     console.log("Você está na nossa lista de espera");
+// }
+// if (notaCandidato < 60) {
+//     console.log("Você foi reprovada(o)");
+// }
+
+const conteudoAcumulado = 5;
+
+if (conteudoAcumulado > 6) {
+    console.log("Se desespere, você tem chance de não dar conta!");
+} else {
+    console.log("Aguenta coração, com esforço ainda dá!");
+}
