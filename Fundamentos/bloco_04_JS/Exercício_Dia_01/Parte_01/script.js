04fad023e5f1e875710ab5205fb3c1d2322bee85
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-08-18 11:54:17 -0300

    Iniciando Javascripit - Variáveis

---
+++ Fundamentos/bloco_04_JS/Exercício_Dia_01/Parte_01/script.js
@@ -0,0 +1,13 @@
+const myName = "Rebeca";
+
+const birthCity = "Recife";
+
+let birthYear = 1990;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 1950;
+
+console.log(birthYear);
+
+birthCity = "Salvador";
