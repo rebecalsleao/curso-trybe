f68995ab969be83dd7f4eb9261b5efc81374db70
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-08-19 09:35:11 -0300

    Ajustes e atualização dos exercícios

---
+++ Fundamentos/bloco_04_JS/Exercício_Dia_01/Parte_06/script.js
@@ -0,0 +1,38 @@
+let a = 5;
+let b = 2;
+let c = 4;
+let numero;
+
+// console.log(a + b);
+
+// console.log(a - b);
+
+// console.log(a * b);
+
+// console.log(a / b);
+
+// console.log(a % b);
+
+// if (a > b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+// if (a > b) {
+//     console.log(a);
+// }
+// if (b > a) {
+//     console.log(b);
+// } else {
+//     console.log(c);
+// }
+
+if (numero === (numero / 2) % 0) {
+    console.log("positive");
+}
+if (numero === (numero / 2) % 1) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
