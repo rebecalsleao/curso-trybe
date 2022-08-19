c69171c30de1b31de2c0108a8f5ec99293bb0ed7
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-08-19 19:11:16 -0300

    Exercícios

--- Fundamentos/bloco_04_JS/Exercício_Dia_01/Parte_06/script.js
+++ Fundamentos/bloco_04_JS/Exercício_Dia_01/Parte_06/script.js
@@ -1,38 +1,127 @@
-let a = 5;
-let b = 2;
-let c = 4;
+1#
+const a = 5;
+const b = 2;
+const c = 4;
 let numero;
 
-// console.log(a + b);
+if (a > b && a > c) {
+    console.log('O maior número é: ' + a + ' (a)');
+  } else if (b > a && b > c) {
+    console.log('O maior número é: ' + b + ' (b)');
+  } else {
+    console.log('O maior número é: ' + c + ' (c)');
+  };
 
-// console.log(a - b);
+2#
 
-// console.log(a * b);
-
-// console.log(a / b);
-
-// console.log(a % b);
-
-// if (a > b) {
-//     console.log(a);
-// } else {
-//     console.log(b);
-// }
-
-// if (a > b) {
-//     console.log(a);
-// }
-// if (b > a) {
-//     console.log(b);
-// } else {
-//     console.log(c);
-// }
-
-if (numero === (numero / 2) % 0) {
-    console.log("positive");
-}
-if (numero === (numero / 2) % 1) {
-    console.log("negative");
+if (a > b) {
+    console.log(a);
 } else {
-    console.log("zero");
+    console.log(b);
 }
+
+3#
+
+if (a > b) {
+    console.log(a);
+}
+if (b > a) {
+    console.log(b);
+}
+if (a > c) {
+    console.log(a);
+}
+if (c > a) {
+    console.log(b);
+}
+if (c > b) {
+    console.log(a);
+}
+if (b > c) {
+    console.log(b);
+}
+
+4#
+let valor = 13;
+if (valor > 0) {
+    console.log("positive");
+} else {
+    console.log("negativo");
+}
+
+5#
+
+let anguloA = 90;
+let anguloB = 40;
+let anguloC = 50;
+
+let angulosTriangulo = anguloA + anguloB + anguloC;
+let anguloPositives = anguloA > 0 && anguloB > 0 && anguloC > 0;
+
+if (anguloPositives) {
+    if (angulosTriangulo === 180) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+} else {
+    console.log("Ângulo inválido");
+}
+
+7#
+
+const nota = 76;
+
+if (nota < 0 || nota > 100) {
+    console.log("Nota errada!");
+} else if (nota >= 90) {
+    console.log("A");
+} else if (nota >= 80) {
+    console.log("B");
+} else if (nota >= 70) {
+    console.log("C");
+} else if (nota >= 60) {
+    console.log("D");
+} else if (nota >= 50) {
+    console.log("E");
+} else {
+    console.log("F");
+}
+
+8#
+
+const num1 = 5;
+const num2 = 7;
+const num3 = 9;
+
+let par = false;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+    let par = true;
+}
+console.log(par);
+
+// 9#
+const num1 = 5;
+const num2 = 3;
+const num3 = 7;
+
+let impar = false;
+
+if ((num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0)) {
+  impar = true;
+};
+console.log(impar);
+
+// 10#
+
+const costOfProduct = 1;
+const saleValue = 3;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  const totalCostOfProduct = costOfProduct * 1.2;
+  const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
