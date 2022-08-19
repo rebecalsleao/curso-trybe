a38d58f33591e5cbb28eae65e20df8e4291889ed
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-08-19 16:53:41 -0300

    Exercícios de JS

---
+++ Fundamentos/bloco_04_JS/Exercício_Dia_02/Parte_04/script.js
@@ -0,0 +1,65 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// // #1
+// for (let index = 0; index < numbers.length; index++) {
+//     console.log(numbers[index]);
+// }
+
+// #2
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     resultado += numbers[index];
+// }
+// console.log(resultado);
+
+// #3
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
+
+// console.log(soma / numbers.length);
+
+// #4
+// let soma = 0;
+// let resultado;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
+
+// resultado = soma / numbers.length;
+
+// if (resultado > 20) {
+//     console.log("Valor maior que 20");
+// } else {
+//     console.log("valor menor ou igual a 20");
+// }
+
+// #5
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     let element = numbers[index];
+//     if (element > resultado) {
+//         resultado = element;
+//     }
+// }
+// console.log(resultado);
+
+// #6
+let resultado = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    let element = numbers[index];
+
+if(resultado === element / 2 % !== 0){
+    console.log(resultado)
+} else {
+    
+}
+    }
+
+console.log(resultado);
