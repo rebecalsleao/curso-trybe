b8aa3dfb8422374ff11e6fc0ba7b124cfa2e10ab
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-08-26 10:03:46 -0300

    Exercícios DOM 5.1 em andamento

---
+++ Fundamentos/bloco_05_DOM_Eventos_Web/Parte_02/script.js
@@ -0,0 +1,5 @@
+document.querySelector(".emergency-tasks").style.backgroundColor = "#fb9f84";
+document.querySelector(".emergency-tasks-style").style.background = "purple";
+
+// document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+// document.querySelector(".emergency-tasks-style").style.backgroundColor = "pur";
