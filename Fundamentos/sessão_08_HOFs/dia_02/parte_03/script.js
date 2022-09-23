ca50ffebdbbcea5803c89df24d356dc16cdfb55c
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-09-23 15:59:41 -0300

    Complemento exercício

--- Fundamentos/sessão_08_HOFs/dia_02/parte_03/script.js
+++ Fundamentos/sessão_08_HOFs/dia_02/parte_03/script.js
@@ -7,3 +7,19 @@ const hasName = (arr, name) => {
 };
 
 console.log(hasName(names, "Maria"));
+
+// Escreva uma função, que dado um array de pessoas e uma idade mínima, retorne true se todas tiverem a idade maior ou igual à mínima e, caso contrário, false. Use every;
+
+const people = [
+    { name: "Mateus", age: 18 },
+    { name: "José", age: 16 },
+    { name: "Ana", age: 23 },
+    { name: "Cláudia", age: 20 },
+    { name: "Bruna", age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+    return arr.every((person) => person.age >= minimumAge);
+};
+
+console.log(verifyAges(people, 18));
