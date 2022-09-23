d40b0ad57da80e5f0dafa33d7350235dc0cb23a0
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-09-23 15:32:49 -0300

    Exercício 8.2 parte 3

---
+++ Fundamentos/sessão_08_HOFs/dia_02/parte_03/script.js
@@ -0,0 +1,9 @@
+// Escreva uma função que recebe dois parâmetros: o primeiro um array de nomes e o segundo, um nome qualquer. Essa função deve retornar true se o segundo parâmetro - um nome qualquer - estiver contido no primeiro parâmetro - array de nomes. Caso não esteja contido, deve retornar false.
+
+const names = ["Mateus", "José", "Ana", "Cláudia", "Bruna"];
+
+const hasName = (arr, name) => {
+    return arr.some((currentName) => currentName === name);
+};
+
+console.log(hasName(names, "Maria"));
